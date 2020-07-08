457ea5611800df4eb092471396744c17beb3e9b9
Jae B <doogie1012@gmail.com>
2020-07-08 21:07:46 +1000

    third commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Contact Site
+
+
+# Testing
+
+```
+GOOS=linux go build -o server && docker-compose --verbose build --no-cache && docker-compose stop && docker-compose up -d && docker-compose logs -f
+```
+
+# Setup Developer Environment on Windows with Docker Toolbox
+
+1) Create a virtual machine
+```
+docker-machine create default
+```
+
+2) Setup your environment in your console to use the newly created VirtualMachine
+```
+eval $(docker-machine env default)
+```
+
+3) Build Go binary
+```
+GOOS=linux go build -o server
+```
+
+3) Build and Run environment
+```
+docker-compose --verbose build --no-cache app && docker-compose stop && docker-compose up -d && docker-compose logs -f
+```
+
+**Side Notes*
+
+* Use `docker-machine ip` to get the machines IP address
+
+## Dependencies Considered
+
+- https://github.com/lib/pq
+	- On the surface this library seems reasonable but the lack of support when it comes to resolving issues or accepting pull-requests means that we'd have to probably fork this if we hit problems with it, rather than being able to contribute fixes back upstream.
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,14 @@
 package app
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
+	"time"
+
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
 )
 
 const port = ":8080"
@@ -28,16 +33,83 @@ func handleHomePage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type Contact struct {
+}
+
 func Start() {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	//dbHost = "192.168.99.100"
+	//dbHost = "db"
+	//dbUser = "admin"
+	//dbPass = "password"
+	//dbPort = "5432"
+
+	shouldEarlyExit := false
+	if dbUser == "" {
+		log.Printf("DB_USER environment variable cannot be empty.")
+		shouldEarlyExit = true
+	}
+	if dbPass == "" {
+		log.Printf("DB_PASSWORD environment variable cannot be empty.")
+		shouldEarlyExit = true
+	}
+	if dbPort == "" {
+		log.Printf("DB_PORT environment variable cannot be empty.")
+		shouldEarlyExit = true
+	}
+	if shouldEarlyExit {
+		os.Exit(1)
+	}
+	// log.Printf("DEBUG: User: %v, Pass: %v, Port: %v\n", dbUser, dbPass, dbPort)
+	db := pg.Connect(&pg.Options{
+		Addr:     dbHost + ":" + dbPort,
+		User:     dbUser,
+		Password: dbPass,
+		OnConnect: func(cn *pg.Conn) error {
+			log.Printf("Successfully connected to SQL server")
+			return nil
+		},
+	})
+	for i := 0; i < 5; i++ {
+		var n int
+		_, err := db.QueryOne(pg.Scan(&n), "SELECT 1")
+		if err == nil {
+			break
+		}
+		log.Printf("Database connection attempt #%d: %v\n", i, err)
+		if i == 4 {
+			log.Println("Unable to connect to database. Stopping app.")
+			os.Exit(1)
+		}
+		time.Sleep(2 * time.Second)
+	}
+	log.Println("Database connection successful")
+	models := []interface{}{
+		(*Contact)(nil),
+	}
+	for _, model := range models {
+		err := db.CreateTable(model, &orm.CreateTableOptions{
+			Temp: true, // temp table
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer db.Close()
+
 	http.HandleFunc("/", handleHomePage)
 	http.HandleFunc("/static/main.css", func(w http.ResponseWriter, r *http.Request) {
 		// Manually serving CSS rather than using http.FileServer because Golang's in-built
 		// detection methods can't really determine if the file is CSS or not.
-		// Chrome complains if you try to load a CSS file with "text/plain". (has errors in console)
+		// Chrome complains if you try to load a CSS file with "text/plain". (has errors in Chrome DevTools)
 		// See "DetectContentType" in the standard library, in file: net\http\sniff.go
 		w.Header().Add("Content-Type", "text/css; charset=utf-8")
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
-	fmt.Printf("Starting server on " + port + "...\n")
+	log.Printf("Starting server on " + port + "...\n")
 	http.ListenAndServe(port, nil)
 }
